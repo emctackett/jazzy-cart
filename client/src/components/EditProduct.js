1099d48efa0e13c085ab1deddd8ae7e64c6ccb16
Kyle <kaledoux@gmail.com>
2021-06-09 14:27:03 -0400

    form extraction, product quantity tracking

--- client/src/components/EditProduct.js
+++ client/src/components/EditProduct.js
@@ -1,40 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
+import Form from './Form';
 
-const EditProduct = ({title, price, quantity, _id, handler, onUpdateProduct}) => {
-  const [newTitle, setNewTitle] = useState(title);
-  const [newPrice, setNewPrice] = useState(price);
-  const [newQuantity, setNewQuantity] = useState(quantity);
+const EditProduct = ({ title, price, quantity, _id, handler, onUpdateProduct }) => {
+	const [ newTitle, setNewTitle ] = useState(title);
+	const [ newPrice, setNewPrice ] = useState(price);
+	const [ newQuantity, setNewQuantity ] = useState(quantity);
 
-  const handleUpdateProduct = (e) => {
-    e.preventDefault();
-    onUpdateProduct({_id, title: newTitle, price: newPrice, quantity: newQuantity});
-  }
-  return (
-    <div class="edit-form">
-      <h3>Edit Product</h3>
-      <form>
-        <div class="input-group">
-          <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value={newTitle} onChange={e => setNewTitle(e.target.value)}/>
-        </div>
-
-        <div class="input-group">
-          <label for="product-price">Price</label>
-          <input type="text" id="product-price" value={newPrice} onChange={e => setNewPrice(e.target.value)}/>
-        </div>
-
-        <div class="input-group">
-          <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value={newQuantity} onChange={e => setNewQuantity(e.target.value)}/>
-        </div>
-
-        <div class="actions form-actions">
-          <a class="button" onClick={handleUpdateProduct}>Update</a>
-          <a class="button" onClick={ handler }>Cancel</a>
-        </div>
-      </form>
-    </div>
-  );
+	const handleUpdateProduct = (e) => {
+		e.preventDefault();
+		onUpdateProduct({ _id, title: newTitle, price: newPrice, quantity: newQuantity });
+	};
+	return (
+		<div class="edit-form">
+			<h3>Edit Product</h3>
+			<Form
+				title={newTitle}
+				price={newPrice}
+				quantity={newQuantity}
+				buttonHandler={handleUpdateProduct}
+				buttonType={'Update'}
+				changeTitle={setNewTitle}
+				changePrice={setNewPrice}
+				changeQuantity={setNewQuantity}
+				onCancel={handler}
+			/>
+		</div>
+	);
 };
 
 export default EditProduct;
