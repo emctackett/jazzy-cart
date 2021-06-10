4c56c1f90494289075995033d51bb72965cae28c
Laura Davies <drljdavies@gmail.com>
2021-06-10 08:38:54 -1000

    Working on deleting products

--- client/src/lib/actions/productActions.js
+++ client/src/lib/actions/productActions.js
@@ -2,4 +2,10 @@ export const productsReceivedSuccess = (products) => {
   return {
     type: "PRODUCTS_RECEIVED", payload: {products}
   }
-};
+}
+
+export const deleteProduct = (productId) => {
+  return {
+    type: "PRODUCT_DELETE", payload: {productId}
+  }
+};;
